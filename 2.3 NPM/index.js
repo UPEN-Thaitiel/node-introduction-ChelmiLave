a06bfa9e6100c74b1ab3abcb72e3fbad14ce9dcb
ChelmiLave <velaangel986@gmail.com>
2025-03-03 07:52:14 -0700

    Clas work

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -3,7 +3,9 @@
 // Install sillyname package with npm install
 
 // Use sillyname package to print a random silly name
-
+var generateName = require('sillyname');
+var sillyName = generateName();
+console.log(sillyName);
 // Install superheroes package with npm install
 
 // Use superheroes package to print a random silly name
